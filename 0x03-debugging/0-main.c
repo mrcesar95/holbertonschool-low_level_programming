de18fdb6e61e06789fc9d676ad77bbb1789ae4f1
Cesar Molina <cesaruxfxtx@gmail.com>
2021-10-05 09:52:23 -0700

    Adding 0-main.c, README.md, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	posiive_or_negativ(i);
+
+	return (0);
+}
