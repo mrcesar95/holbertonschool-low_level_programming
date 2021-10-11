fbecca566dc4d12f13ba6765014094a28f5bdf1a
Cesar Molina <cesaruxgxtx@gmail.com>
2021-10-11 11:53:16 -0500

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_rev - Entry point
+ *
+ *@s: Ponit to the string
+ *
+ *
+ *
+ *Return: void
+ */
+
+void print_rev(char *s)
+{
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	s--;
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
