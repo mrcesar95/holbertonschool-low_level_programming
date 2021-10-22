be52869a930a60994b67c6780148d1fef8a5745c
Cesar Molina <cesaruxgxtx@gmail.com>
2021-10-22 13:37:13 -0500

    Fixing up 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,12 +3,12 @@
 
 /**
  * main - print the number of arguments passed to the program
- *@argc: number of arguments
- *@argv: array of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[] __atribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
