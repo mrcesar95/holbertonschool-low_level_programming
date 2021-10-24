55a1d3e5d93c6e7c8537ab2d778442250502a597
Cesar Molina <cesaruxgxtx@gmail.com>
2021-10-24 18:05:41 -0500

    Creating the library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_puts - entry point
+*
+*@str : Ponit to the string
+*
+*
+*Return: Length of string
+*/
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
