a2c0d90e5c7bdd3bfe999afe37ef8706c59dfe27
Cesar Molina <cesaruxfxtx@gmail.com>
2022-01-10 15:18:05 -0500

    Fixing up file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#!/bin/bash
 wget -O /tmp/cracking_output.so https://github.com/mrcesar95/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/cracking_output.so
 export LD_PRELOAD=/tmp/cracking_output.so
